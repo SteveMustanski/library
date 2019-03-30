f6ac763b9e68da67b361b68cb5c1762d8866a165
SteveMustanski <steve.mustanski@gmail.com>
2019-03-30 13:55:53 -0400

    added path and html file at /

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -9,7 +10,7 @@ const port = 3000;
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.send('Hello from the library app');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(port, () => {
