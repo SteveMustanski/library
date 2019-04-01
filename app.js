806b1e650727173389d1f759644f8bbd09b5564d
SteveMustanski <steve.mustanski@gmail.com>
2019-04-01 15:42:46 -0400

    passed the nav in as an argument to the router funtion so it didn't have to be copied everywhere

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = require('./src/routes/bookRoutes');
+
+const nav = [
+  { link: '/books', title: 'Books' },
+  { link: '/authors', title: 'Authors' },
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,47 +2,43 @@ const express = require('express');
 
 const bookRouter = express.Router();
 
-const books = [
-  {
-    title: "Hitchiker's Guide to the Galaxy",
-    genre: 'Science Fiction',
-    author: 'Douglas Adams',
-    read: false,
-  },
-  {
-    title: 'The Lord of the Rings',
-    genre: 'Fantasy',
-    author: 'JRR Tolkien',
-    read: false,
-  },
-  {
-    title: 'Atlas Shrugged',
-    genre: 'Fiction',
-    author: 'Ayn Rand',
-    read: false,
-  },
-];
-
-bookRouter.route('/').get((reg, res) => {
-  res.render('bookList', {
-    nav: [
-      { link: '/books', title: 'Books' },
-      { link: '/authors', title: 'Authors' },
-    ],
-    title: 'Library',
-    books,
+function router(nav) {
+  const books = [
+    {
+      title: "Hitchiker's Guide to the Galaxy",
+      genre: 'Science Fiction',
+      author: 'Douglas Adams',
+      read: false,
+    },
+    {
+      title: 'The Lord of the Rings',
+      genre: 'Fantasy',
+      author: 'JRR Tolkien',
+      read: false,
+    },
+    {
+      title: 'Atlas Shrugged',
+      genre: 'Fiction',
+      author: 'Ayn Rand',
+      read: false,
+    },
+  ];
+  bookRouter.route('/').get((reg, res) => {
+    res.render('bookList', {
+      nav,
+      title: 'Library',
+      books,
+    });
   });
-});
-bookRouter.route('/:id').get((req, res) => {
-  const { id } = req.params;
-  res.render('book', {
-    nav: [
-      { link: '/books', title: 'Books' },
-      { link: '/authors', title: 'Authors' },
-    ],
-    title: 'Library',
-    book: books[id],
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;
+    res.render('book', {
+      nav,
+      title: 'Library',
+      book: books[id],
+    });
   });
-});
+  return bookRouter;
+}
 
-module.exports = bookRouter;
+module.exports = router;
