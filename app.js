9bb6faa1a2f3ca473ec9b0b38f14d02d14c44de3
SteveMustanski <steve.mustanski@gmail.com>
2019-04-02 14:17:14 -0400

    added admin route

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const nav = [
 ];
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -30,6 +31,8 @@ app.use(
 );
 
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
+
 app.get('/', (req, res) => {
   res.render('index', {
     nav,
