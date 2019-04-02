342ae5f02793b165ca1247b8c019503d555efc7e
SteveMustanski <steve.mustanski@gmail.com>
2019-04-02 14:35:51 -0400

    added connection to mongo and admin route to insert books

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,49 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
 
 const adminRouter = express.Router();
 
+const books = [
+  {
+    title: "Hitchiker's Guide to the Galaxy",
+    genre: 'Science Fiction',
+    author: 'Douglas Adams',
+    read: false,
+  },
+  {
+    title: 'The Lord of the Rings',
+    genre: 'Fantasy',
+    author: 'JRR Tolkien',
+    read: false,
+  },
+  {
+    title: 'Atlas Shrugged',
+    genre: 'Fiction',
+    author: 'Ayn Rand',
+    read: false,
+  },
+];
+
 function router(nav) {
   adminRouter.route('/').get((req, res) => {
-    res.send('instering books');
+    const url = 'mongoDB://localhost:27017';
+    const dbName = 'libraryApp';
+
+    // eslint-disable-next-line wrap-iife
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connected to Mongo Server');
+        const db = client.db(dbName);
+        const response = await db.collection('books').insertMany(books);
+        res.json(response);
+      } catch (err) {
+        debug(err.stack);
+      }
+      client.close();
+    })();
   });
   return adminRouter;
 }
