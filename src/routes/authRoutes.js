3739d92e80283017531612b1d6febdab86b21894
SteveMustanski <steve.mustanski@gmail.com>
2019-04-04 14:47:52 -0400

    added create user to write user to mongo db

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,36 @@
 const express = require('express');
-// const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
 
 const authRouter = express.Router();
 
 function router() {
   authRouter.route('/signUp').post((req, res) => {
+    const { username, password } = req.body;
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    // eslint-disable-next-line wrap-iife
+    (async function addUser() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connected to mongo db');
+
+        const db = client.db(dbName);
+        const col = db.collection('users');
+        const user = { username, password };
+        const results = await col.insertOne(user);
+        debug(results);
+        // create user
+        req.login(results.ops[0], () => {
+          res.redirect('/auth/profile');
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+    })();
     debug(req.body);
-    // create user
-    req.login(req.body, () => {
-      res.redirect('/auth/profile');
-    });
   });
   authRouter.route('/profile').get((req, res) => {
     res.json(req.user);
