d0a33628895df0bfba4092512f3c8fc8651ec848
SteveMustanski <steve.mustanski@gmail.com>
2019-04-10 12:46:35 -0400

    added hard coded axios call to get book details and added to page

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,7 +1,32 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray: false });
+
 function goodreadsService() {
   function getBookById() {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'our description' });
+      axios
+        .get(
+          'https://www.goodreads.com/book/show/386162.xml?key=Yn0cojbDqUH25UoCMeg',
+        )
+        // eslint-disable-next-line arrow-parens
+        .then(response => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        // eslint-disable-next-line arrow-parens
+        .catch(error => {
+          reject(error);
+          debug(error);
+        });
     });
   }
   return { getBookById };
